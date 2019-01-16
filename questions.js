04273ac78a30c8115b07ba43036212946608e56b
Junior Rueda <jruedalf@gmail.com>
2019-01-16 17:13:45 -0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,126 @@
+var questions = [{
+            question: "¿Cuál es nuestro sentido del propósito?",
+            choices: ["Todo lo que hagamos o digamos hagamos o digamos, sea para ayudar y ser feliz al otro.", "Todo lo que queremos hacer ; es para ser feliz al Liderman", "Todo lo que hace feliz al Liderman; es ayudar a ser feliz al otro", "Todo que digamos o hagamos; es para ayudar al otro", "Todo el bien que hagamos nos hace felices si ayudamos al otro."],
+            correctAnswer: 0
+        }, 
+        {
+            question: "En que parte del pasaporte contempla la siguiente expresión: Brindar un servicio especializado, priorizando la calidad, cuidado del medio ambiente, protección de la vida y la salud; para superar las necesidades conocidas y no conocidas aun por nuestros clientes.",
+            choices: ["Objetivos estratégicos", "Misión", "Visión", "Sentido del propósito", "Consignas permanentes"],
+            correctAnswer: 2
+        },
+        {
+            question: "¿Cuántos y cuáles son los valores Liderman?",
+            choices: ["7; Honestidad, creatividad e innovación, solidaridad, comunicación, vocación por la felicidad, trabajo en equipo, sentido del propósito.", 99, 108, 134, 156],
+            correctAnswer: 0
+        },
+        {
+            question: "El robo, los accidentes causados por manipulación de armas, abandonar el puesto de servicio son:",
+            choices: ["Sentido del propósito", "Consignas permanentes", "Lo que no perdona el cliente", "Misión", "Visión"],
+            correctAnswer: 2
+        },
+        {
+            question: "¿Cuáles son los pilares de la cultura?",
+            choices: ["5 (Sentido del propósito, Misión y Visión, Valores, lo que no perdona el cliente, consignas permanentes).", "Valores, folder LARS y SGI, objetivos estratégicos, calidad operativa, consignas permanentes.", "El robo, la mentira, la mala intención en actos y palabras, trabajar en estado etílico y abandonar el puesto de servicio.", "Misión, visión, objetivos estratégicos, política integrada.", "Honestidad, Creatividad e innovación, solidaridad, comunicación, vocación por la felicidad, trabajo en equipo, sentido del propósito."],
+            correctAnswer: 0
+        },
+        {
+            question: "No es una consigna permanente:",
+            choices: ["Me presentare al servicio puntual, limpio y con el equipo completo.", "Mi actitud será siempre de alerta y no de confianza.", "Descuidos leves, frecuentes o repetitivos que desgastan el servicio.", "Nunca jugare ni hare uso indebido de mi arma o equipo.", "Nunca me descuidare por pensar o hacer cosas ajenas al servicio"],
+            correctAnswer: 2
+        }, {
+            question: "Es la misión de Liderman: Atender con máxima eficiencia, los asuntos de seguridad delegados por nuestros clientes, para que se puedan enfocar en su operación principal; destacándoles el mejor talento humano respaldado por la cultura Liderman.",
+            choices: ["Verdadero", "Falso"],
+            correctAnswer: 0
+        }, {
+            question: "¿Cuántos anexos contiene el Folder Lars?",
+            choices: ["5", "9", "7", "3", "N.A."],
+            correctAnswer: 2
+        }, {
+            question: "¿Cuáles son los puntos de calidad operativa?",
+            choices: ["Selección, presentación, capacitación, supervisión, valor agregado.", "Incrementar, controlar, mejorar.", "Anexo 1, anexo 2, anexo 3, anexo 4, anexo 5, anexo 6, anexo 7", "Sentido del propósito, visión y misión, valores, lo que no perdona el cliente, consignas permanentes.", "Honestidad, creatividad e innovación, solidaridad, comunicación, vocación por la felicidad, trabajo en equipo y sentido del humor."],
+            correctAnswer: 0
+        }, {
+            question: "En que sección del pasaporte figura: “Doy servicio de seguridad, cumpliendo con las disposiciones del cliente, cuidando el medio ambiente, cuidando mi integridad y salud, usando mis EPP, mejorando día a día porque Liderman me capacita.”",
+            choices: ["Misión", "Visión", "Objetivos estratégicos", "Política integrada", "Consignas permanentes"],
+            correctAnswer: 3
+        }, {
+            question: "¿Cómo usar un extintor?",
+            choices: ["Descolgar, quitar anilla de seguridad, presionar palanca, dirigir la descarga a la base de la llama."],
+            correctAnswer: 0
+        }, {
+            question: "¿Cuáles son las clases de extintores?",
+            choices: ["Agua presurizada, agua desionizada, Co2, PQS, Acetato de potasio.", "A, B, C, D y K.", "Oxígeno, calor, combustible, Reacción en cadena.", "N.A.", "T.A."],
+            correctAnswer: 3
+        }, {
+            question: "¿Cuáles son las clases de incendio?",
+            choices: ["A, B, C, D Y F", "A, B, C, D y K.", "Oxígeno, calor, combustible, Reacción en cadena.", "N.A.", "AB, AC, AD,AK"],
+            correctAnswer: 1
+        }, {
+            question: ". ¿Cuál de los siguientes “Identifica, prioriza y gestiona los rasgos ambientales, como parte de sus prácticas de negocios habituales”?",
+            choices: ["ISO 9001- 2015 (Sistema de gestión de calidad)", "OHSAS 18001- 2007 (Sistema de gestión en seguridad y salud)", "BASC", "ISO 14001 – 2015 (Sistema de gestión ambiental)", "Todas las anteriores"],
+            correctAnswer: 3
+        }, {
+            question: "¿Qué clase de extintor se utiliza para los incendios producidos por equipos o maquinas energizadas?",
+            choices: ["Agua presurizada", "Acetato de potasio", "Agua desionizada", "CO2", "PQS"],
+            correctAnswer: [2, 3, 4]
+        }, {
+            question: "¿Cuál de las siguientes “regula el sistema con la finalidad de brindar un servicio de calidad”?",
+            choices: ["ISO 9001 – 2015", "BASC", "ISO 14001 – 2015", "OHSAS 18001 – 2007", "T.A."],
+            correctAnswer: 0
+        }, {
+            question: "No es un valor de Liderman",
+            choices: ["Comunicación", "Vocación por un buen servicio", "Creatividad e innovación", "Solidaridad", "Sentido del humor"],
+            correctAnswer: 1
+        }, {
+            question: "¿Cuál de los siguientes no es un objetivo estratégico de Liderman?",
+            choices: ["Incrementar: la competitividad en el mercado nacional al brindar servicios de seguridad privada.", "Controlar: el consumo de los servicios básicos (luz, agua, papel)", "Mejorar: las condiciones de seguridad, salud y ambiente de trabajo para todos los colaboradores.", "Doy servicio de seguridad, cumpliendo las disposiciones del cliente, cuidando el medio ambiente, cuidando mi integridad y salud, usando mi EPP, mejorando día a día porque Liderman me capacita.", "Ninguna de la anteriores"],
+            correctAnswer: 3
+        }, {
+            question: "Registro de no conforme, listado de registro, propiedad del cliente, le pertenece a: ",
+            choices: ["Carpeta / Folder LARS", "Consignas permanentes", "Objetivos estratégicos", "Carpeta / Folder SGI", "Puntos de calidad operativa"],
+            correctAnswer: 3
+        }, {
+            question: "¿Qué es lo que perdona el cliente?",
+            choices: ["El robo", "La mentira", "Trabajar en estado etílico", "Abandonar el puesto de servicio", "Ninguna de las anteriores"],
+            correctAnswer: 4
+        }, {
+            question: "¿Cuáles con nuestros objetivos estratégicos?",
+            choices: ["Planificar, elaborar y ejecutar", "Controlar, incrementar, reaccionar", "Incrementar, controlar y mejorar", "Seleccionar, supervisar, capacitar."],
+            correctAnswer: 2
+        }, {
+            question: "¿Cuántas son nuestras consignas permanentes?",
+            choices: ["10", "7", "5", "12", "8"],
+            correctAnswer: 3
+        }, {
+            question: ". ¿Cuál de las siguientes no es una consigna permanente?",
+            choices: ["Nunca me descuidare por pensar o hacer cosas ajenas al servicio.", "Respetaré y hare cumplir las disposiciones de la empresa a la cual estoy asignado.", "En caso de robo nunca debo hacer uso de mi arma.", "Nunca jugare ni hare uso indebido de mi arma o equipo.", "Me esmerare y pondré todo de mí en el servicio."],
+            correctAnswer: 2
+        }, {
+            question: "¿Qué contempla el anexo 7 de la carpeta / folder LARS?",
+            choices: ["Números de emergencia.", "Valores", "Matriz de riesgo medio ambiental (IAI).", "Inventario de extintores y gabinete contra incendio.", "Análisis de riesgo"],
+            correctAnswer: 2
+        }, {
+            question: "¿Cuál de los siguientes no es un elemento que compone el fuego?",
+            choices: ["Oxigeno", "Calor", "Combustible", "Hidrógeno", "Reacción en cadena."],
+            correctAnswer: 3
+        }, {
+            question: "¿Qué produce el incendio de clase K?",
+            choices: ["Aceites y grasas vegetales", "Combustibles sólidos (madera, papel, plástico)", "Equipos y maquinas energizadas", "Combustibles líquidos (petróleo, diésel, GLP, gasolina, bencina, alcohol, otros)", "Metales combustibles inflamables (potasio, sodio, magnesio, otros)"],
+            correctAnswer: 0
+        }, {
+            question: "¿Ante qué situación se aplica la maniobra de Heimlich?",
+            choices: ["Hemorragia", "Atragantamiento", "Fractura", "Reanimación cardio pulmonar (RCP)", "Caída"],
+            correctAnswer: 1
+        }, {
+            question: "Dentro de la seguridad contra incendios ¿Cómo se define a la reacción en cadena?",
+            choices: ["Se encuentra en el ambiente.", "Producido por combustibles líquidos y gas inflamable.", "Es la combinación de calor, combustible y oxígeno", "Es el elemento principal que genera la combustión, puede encontrarse en estado sólido, liquido o gaseoso."],
+            correctAnswer: 2
+        }, {
+            question: "¿Cuál de los siguientes no es parte del Folder LARS?",
+            choices: ["Matriz Medio Ambiental", "Plano de extintores y señalética", "Registro de no conformes", "Análisis de riesgos", "Inventario de extintores y gabinete contra incendio."],
+            correctAnswer: 2
+        }, {
+            question: "¿Cuál de los siguientes no figura en “lo que no perdona el cliente”?",
+            choices: ["Los descuidos que ocasionan fallas o problemas graves", "Abandonar el puesto de servicio", "Los accidentes causados por descuidos del cliente", "El robo", "Trabajar en estado etílico"],
+            correctAnswer: 2
+        }];
